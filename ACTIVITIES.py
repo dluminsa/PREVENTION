8b55f1519b6c127eb52a3d1c5ace9c8b004d7542
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-10-30 17:33:16 +0300

    Update ACTIVITIES.py

--- ACTIVITIES.py
+++ ACTIVITIES.py
@@ -9,8 +9,8 @@ from datetime import datetime, date
 st.set_page_config(
      page_title= 'ACTIVITY TRACKER'
 )
-st.write('BEING UPDATED, WILL RETURN AFTER THE NEW BUDGETS')
-st.stop()
+# st.write('BEING UPDATED, WILL RETURN AFTER THE NEW BUDGETS')
+# st.stop()
 
 CLUSTER = {
     "KALANGALA": ["KALANGALA"],
